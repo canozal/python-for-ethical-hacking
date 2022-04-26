6f2164cc9b70882bdeca710fbc98b2ec4dc4325a
can.ozal <cannozall@gmail.com>
2022-04-26 13:00:06 +0300

    add user input

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -1,5 +1,15 @@
 import subprocess
 
-subprocess.call("ifconfig wlan0 down", shell=True)
-subprocess.call("ifconfig wlan0 hw ether 00.11.22.33.44.55.66", shell=True)
-subprocess.call("ifconfig wlan0 up", shell=True)
+interface = input("[*] Enter interface: ")
+new_mac = input("[*] Enter new MAC address: ")
+
+print("[*] Changing MAC address for " + interface + " to " + new_mac)
+
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
+
+
+
+
+
