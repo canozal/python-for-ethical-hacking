248281cf5ae11976858a2b788413814ffefd5269
can.ozal <cannozall@gmail.com>
2022-04-27 12:59:11 +0300

    add random functionality

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -1,16 +1,31 @@
 import subprocess
 import optparse
+import random
 
+
+def random_mac():
+    mac = [0x00, 0x16, 0x3e,
+           random.randint(0x00, 0x7f),
+           random.randint(0x00, 0xff),
+           random.randint(0x00, 0xff)]
+    return ':'.join(map(lambda x: "%02x" % x, mac))
 
 def get_arguments():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address")
     parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+    parser.add_option("-r", "--random", dest="random", help="Random MAC address")
+
     (options, arguments) = parser.parse_args()
     if not options.interface:
         parser.error("[-] Please specify an interface, use --help for more info.")
     if not options.new_mac:
         parser.error("[-] Please specify a new MAC address, use --help for more info.")
+    if options.random and options.new_mac:
+        parser.error("[-] Please specify only one option, use --help for more info.")
+    if options.random:
+        options.new_mac = random_mac()
+
     return options
 
 
