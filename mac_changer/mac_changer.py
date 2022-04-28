a2c262ac0dc0bb2b2d60de39a933b315e0085715
can <cannozall@gmail.com>
2022-04-28 21:19:34 +0300

    refactoring

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -36,16 +36,28 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
+
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] Could not read MAC address.")
+
 
 options = get_arguments()
 
-# change_mac(options.interface, options.new_mac)
+old_mac = get_current_mac(options.interface)
 
-ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
+change_mac(options.interface, options.new_mac)
 
-mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result.decode("utf-8"))
+new_mac = get_current_mac(options.interface)
 
-if mac_address_search_result:
-    print("[+] Current MAC address: " + mac_address_search_result.group(0))
+if new_mac == options.new_mac:
+    print("[+] MAC address was successfully changed from " + old_mac + " to " + new_mac)
 else:
-    print("[-] Could not read MAC address.")
+    print("[-] MAC address did not get changed.")
+
+
+
